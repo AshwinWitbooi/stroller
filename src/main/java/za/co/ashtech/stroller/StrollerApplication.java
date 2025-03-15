ac0bd231123ba5714af0d26e4db16542e83fb8f3
Ashwin <ashwin.witbooi@gmail.com>
2025-03-15 13:35:11 +0200

    initial project creation commit

---
+++ src/main/java/za/co/ashtech/stroller/StrollerApplication.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.stroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/ashtech/stroller/StrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.stroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
