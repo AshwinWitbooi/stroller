4c6ea8af92888cee080571f96d913ceba49ad4b3
Ashwin <ashwin.witbooi@gmail.com>
2025-04-28 14:39:49 +0200

    Add application info for both running application locally and from command line

--- src/main/java/za/co/ashtech/stroller/config/ActuatorInfo.java
+++ src/main/java/za/co/ashtech/stroller/config/ActuatorInfo.java
@@ -1,12 +1,14 @@
 package za.co.ashtech.stroller.config;
 
-import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.jar.Attributes;
+import java.util.jar.JarFile;
 import java.util.jar.Manifest;
+
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
@@ -25,21 +27,56 @@ public class ActuatorInfo implements InfoContributor {
     @Override
     public void contribute(Info.Builder builder) {
         
-		URLClassLoader cl = (URLClassLoader) StrollerApplication.class.getClassLoader();
-		log.debug("::: -> Get class loader");
-		URL url = cl.findResource("META-INF/MANIFEST.MF");
-		log.debug("::: -> Get manifest file");
-		Manifest manifest;
 		try {
-			manifest = new Manifest(url.openStream());
-			Attributes attr = manifest.getMainAttributes();
 			
-			Iterator<Entry<Object, Object>> entrySet = attr.entrySet().iterator();	
-			 
-			entrySet.forEachRemaining(e -> {builder.withDetail(e.getKey().toString(), e.getValue().toString());});
+			//Get path to where code is excuting from	
+	    	String pathToJar = StrollerApplication.class.getProtectionDomain()
+						        .getCodeSource()
+						        .getLocation()
+						        .toURI()
+						        .toString();
+	    	log.debug("::: -> Path To JAR {}",pathToJar);
+	    	
+	    	//Check whether path is from Spring Boot Jar running
+			if(pathToJar.contains("/!BOOT-INF/classes/!/")) {
+	
+				pathToJar = pathToJar.replaceAll("jar:nested:/", "");
+				log.debug("::: -> Remove prefix from Jar path {}",pathToJar);
+				pathToJar = pathToJar.replaceAll("/!BOOT-INF/classes/!/", "");
+				log.debug("::: -> Remove postfix from Jar path {}",pathToJar);
+				
+				try (JarFile jarFile = new JarFile(pathToJar)) {
+		            Manifest manifest = jarFile.getManifest();
+		            if (manifest != null) {
+		                manifest.getMainAttributes().forEach((key, value) ->
+		                
+		                builder.withDetail(key.toString(), value.toString()));
+		                
+		            } else {
+		                new NullPointerException("No manifest found in the JAR file.");
+		            }
+		        }
+			}else {
+				URLClassLoader cl = (URLClassLoader) StrollerApplication.class.getClassLoader();
+				log.debug("::: -> Get class loader");
+				URL url = cl.findResource("META-INF/MANIFEST.MF");
+				log.debug("::: -> Jar URL {}",url.getFile());
+				log.debug("::: -> Get manifest file");
+				Manifest manifest;
+				
+				manifest = new Manifest(url.openStream());
+				Attributes attr = manifest.getMainAttributes();
+				
+				Iterator<Entry<Object, Object>> entrySet = attr.entrySet().iterator();	
+				 
+				entrySet.forEachRemaining(e -> {builder.withDetail(e.getKey().toString(), e.getValue().toString());});				  
+
+			}
 			
-		} catch (IOException e) {
-			log.error(":::	-> Error getting running jar manifest file",e);
+		} catch (URISyntaxException e) {
+			log.error(e.getLocalizedMessage());
+		}catch (Exception e) {
+			log.error(e.getLocalizedMessage());
 		}
     }
 }
