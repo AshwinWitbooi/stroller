6c96197de8387b49def14f7ccc819a42209a986f
Ashwin <ashwin.witbooi@gmail.com>
2025-05-25 20:15:42 +0200

    update response return ResponseEntity with body

--- src/main/java/za/co/ashtech/stroller/controller/StrollerController.java
+++ src/main/java/za/co/ashtech/stroller/controller/StrollerController.java
@@ -1,6 +1,7 @@
 package za.co.ashtech.stroller.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,8 @@ public class StrollerController {
     private StrollerService strollerService;
 
     @GetMapping("stroll")
-    public Stroll getAllCustomers() {
-        return strollerService.getRandomStrol();
+    public ResponseEntity<Stroll> getAllCustomers() {
+        return ResponseEntity.ok(strollerService.getRandomStroll());
     }
 
 }
--- src/test/java/za/co/ashtech/stroller/services/ServiceLayerTests.java
+++ src/test/java/za/co/ashtech/stroller/services/ServiceLayerTests.java
@@ -15,14 +15,26 @@ class ServiceLayerTests {
 
 	@Autowired
 	private StrollerService strollerService;
+	@Autowired
+	private StrollerAdminService strollerAdminService;
 
 	@Test
-	void testStrollerRepository() {
+	void testGetRandomStroll() {
 		assertNotNull(strollerService);
 		
-		Stroll stroll = strollerService.getRandomStrol();
+		Stroll stroll = strollerService.getRandomStroll();
 		assertNotNull(stroll);
 		log.info(stroll.toString());
+	}
+	
+	@Test
+	void testStrollerRepository() {
+		assertNotNull(strollerAdminService);
+		
+		Stroll stroll = new Stroll("unitName", "unitTestLoc", "132.33", "36.664");
+		Stroll strollResponse = strollerAdminService.addStroll(stroll);
+		assertNotNull(strollResponse);
+		log.info(strollResponse.toString());
 	}
 
 }
