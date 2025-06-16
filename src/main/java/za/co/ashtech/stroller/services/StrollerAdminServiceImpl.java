87b3f26972b022ae0ab8667501d2951ed4fd00d3
Ashwin <ashwin.witbooi@gmail.com>
2025-06-16 21:38:30 +0200

    Add exception handling

--- src/main/java/za/co/ashtech/stroller/services/StrollerAdminServiceImpl.java
+++ src/main/java/za/co/ashtech/stroller/services/StrollerAdminServiceImpl.java
@@ -17,13 +17,20 @@ public class StrollerAdminServiceImpl implements StrollerAdminService {
 	@Override
 	public Stroll addStroll(Stroll stroll) throws StrollerServiceException {
 
-		/*
-		 *persist database entity
-		 *values mapped from request entity
-		 */
-		za.co.ashtech.stroller.db.entities.Stroll dbo = strollerRepository.save(new za.co.ashtech.stroller.db.entities.Stroll(stroll.getName(), stroll.getLocation(),Double.valueOf(stroll.getLatitude()),Double.valueOf(stroll.getLatitude())));
-		log.debug("-D DATABASE ENTITY SUCCESSFULLY PERSISTED");
-		
+
+		za.co.ashtech.stroller.db.entities.Stroll dbo = null;
+		try {
+			/*
+			 *persist database entity
+			 *values mapped from request entity
+			 */
+			dbo = strollerRepository.save(new za.co.ashtech.stroller.db.entities.Stroll(stroll.getName(), stroll.getLocation(),Double.valueOf(stroll.getLatitude()),Double.valueOf(stroll.getLatitude())));
+			log.debug("-D DATABASE ENTITY SUCCESSFULLY PERSISTED");
+		} catch (NumberFormatException e) {
+			throw new StrollerServiceException("Error saving stroll", e);
+		}catch (Exception e){
+			throw new StrollerServiceException(e);
+		}
 		return new Stroll(dbo.getName(), dbo.getLocation(),Double.toString(dbo.getLatitude()),Double.toString(dbo.getLongitude()));
 	}
 
@@ -31,22 +38,33 @@ public class StrollerAdminServiceImpl implements StrollerAdminService {
 	public Stroll updateStroll(Long id, Stroll stroll) throws StrollerServiceException {
 
 
-		za.co.ashtech.stroller.db.entities.Stroll existing = strollerRepository.findById(id)
-                .orElseThrow(() -> new NullPointerException("Stroll not found with id: " + id));
-		
-		existing.setName(stroll.getName());
-		existing.setLocation(stroll.getLocation());
-		existing.setLatitude(Double.valueOf(stroll.getLatitude()));
-		existing.setLongitude(Double.valueOf(stroll.getLongitude()));
-		
-		za.co.ashtech.stroller.db.entities.Stroll updatedStroll = strollerRepository.save(existing);
+		za.co.ashtech.stroller.db.entities.Stroll updatedStroll = null;
+		try {
+			za.co.ashtech.stroller.db.entities.Stroll existing = strollerRepository.findById(id)
+			        .orElseThrow(() -> new NullPointerException("Stroll not found with id: " + id));
+			
+			existing.setName(stroll.getName());
+			existing.setLocation(stroll.getLocation());
+			existing.setLatitude(Double.valueOf(stroll.getLatitude()));
+			existing.setLongitude(Double.valueOf(stroll.getLongitude()));
+			
+			updatedStroll = strollerRepository.save(existing);
+		} catch (NumberFormatException e) {
+			throw new StrollerServiceException(e);
+		} catch (NullPointerException e) {
+			throw new StrollerServiceException(e);
+		}
 		
 		return new Stroll(updatedStroll.getName(), updatedStroll.getLocation(), updatedStroll.getLongitude().toString(), updatedStroll.getLongitude().toString());
 	}
 
 	@Override
 	public void deleteStroll(Long id) throws StrollerServiceException {
-		strollerRepository.deleteById(id);
+		try {
+			strollerRepository.deleteById(id);
+		} catch (Exception e) {
+			throw new StrollerServiceException(e);
+		}
 	}
 
 
