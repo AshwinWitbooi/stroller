90e2374d0f2e06aee5d539f50e711443cbe0b99a
Ashwin <ashwin.witbooi@gmail.com>
2025-07-06 11:14:24 +0200

    Add validateUserIdRequestParam method to validate request contain required userid param

--- src/main/java/za/co/ashtech/stroller/controller/AdminStrollerController.java
+++ src/main/java/za/co/ashtech/stroller/controller/AdminStrollerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.ashtech.stroller.controller.entities.Stroll;
 import za.co.ashtech.stroller.services.StrollerAdminService;
@@ -16,25 +17,30 @@ import za.co.ashtech.stroller.util.StrollerServiceException;
 
 @RestController
 @RequestMapping("admin/api/v1")
-public class AdminStrollerController {
+public class AdminStrollerController extends BaseController{
 
     @Autowired
     private StrollerAdminService strollerAdminService;
 
     @PostMapping("stroll")
-    public ResponseEntity<Void> addStroll(@RequestBody Stroll stroll) throws StrollerServiceException{
+    public ResponseEntity<Void> addStroll(@RequestBody Stroll stroll,@RequestParam(required = false) String userId) throws StrollerServiceException{
+    	//validate userId request param
+    	validateUserIdRequestParam(userId);
     	strollerAdminService.addStroll(stroll);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-    
-    
+        
     @PutMapping("stroll/{id}")
-    public ResponseEntity<Stroll> updateStroll(@PathVariable Long id,@RequestBody Stroll stroll) throws StrollerServiceException{
-        return ResponseEntity.ok(strollerAdminService.updateStroll(id, stroll));
+    public ResponseEntity<Stroll> updateStroll(@PathVariable Long id,@RequestBody Stroll stroll,@RequestParam(required = false) String userId) throws StrollerServiceException{
+    	//validate userId request param
+    	validateUserIdRequestParam(userId);
+    	return ResponseEntity.ok(strollerAdminService.updateStroll(id, stroll));
     }
     
     @DeleteMapping("stroll/{id}")
-    public ResponseEntity<Void> deleteStroll(@PathVariable Long id) throws StrollerServiceException{
+    public ResponseEntity<Void> deleteStroll(@PathVariable Long id,@RequestParam(required = false) String userId) throws StrollerServiceException{
+    	//validate userId request param
+    	validateUserIdRequestParam(userId);
     	strollerAdminService.deleteStroll(id);
         return ResponseEntity.noContent().build();
     }
