db1c5acf2526703a682624399a2104a3c8180504
Ashwin <ashwin.witbooi@gmail.com>
2025-07-06 11:16:35 +0200

    Add validateUserIdRequestParam method to check request params

--- src/main/java/za/co/ashtech/stroller/controller/StrollerController.java
+++ src/main/java/za/co/ashtech/stroller/controller/StrollerController.java
@@ -4,20 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import za.co.ashtech.stroller.controller.entities.Stroll;
 import za.co.ashtech.stroller.services.StrollerService;
+import za.co.ashtech.stroller.util.StrollerServiceException;
 
 @RestController
 @RequestMapping("/api/v1")
-public class StrollerController {
+public class StrollerController extends BaseController{
 
     @Autowired
     private StrollerService strollerService;
 
     @GetMapping("stroll")
-    public ResponseEntity<Stroll> getAllCustomers() {
+    public ResponseEntity<Stroll> getAllCustomers(@RequestParam(required = false) String userId) throws StrollerServiceException{
+    	//validate userId request param
+    	validateUserIdRequestParam(userId);
         return ResponseEntity.ok(strollerService.getRandomStroll());
     }
 
