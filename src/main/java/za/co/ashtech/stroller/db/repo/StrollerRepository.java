9908b50febb021e8012580f80767820b4c074502
Ashwin <ashwin.witbooi@gmail.com>
2025-10-25 10:10:29 +0200

    Add findbyname custom query

--- src/main/java/za/co/ashtech/stroller/db/repo/StrollerRepository.java
+++ src/main/java/za/co/ashtech/stroller/db/repo/StrollerRepository.java
@@ -2,6 +2,8 @@ package za.co.ashtech.stroller.db.repo;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import za.co.ashtech.stroller.db.entities.Stroll;
@@ -10,5 +12,6 @@ import za.co.ashtech.stroller.db.entities.Stroll;
 public interface StrollerRepository extends JpaRepository<Stroll, Long> {
 	
 	Optional<Stroll> findByStrollId(Integer strollId);
-
+	@Query("SELECT s FROM Stroll s WHERE UPPER(s.strollName) = UPPER(:strollName)")
+	Optional<Stroll> findByStrollName(@Param("strollName") String strollName);	
 }
