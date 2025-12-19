69b6a16efa06a5f72d0c4a613820d87b0ae93c87
Ashwin <ashwin.witbooi@gmail.com>
2025-12-19 18:16:17 +0200

    update security for local testing

--- src/main/java/za/co/ashtech/stroller/config/LocalSecurityConfig.java
+++ src/main/java/za/co/ashtech/stroller/config/LocalSecurityConfig.java
@@ -57,10 +57,10 @@ public class LocalSecurityConfig {
 		http
 			.csrf(csrf -> csrf.disable())
 	    	.cors(Customizer.withDefaults()) 
-			.securityMatcher("/admin/api/v1/stroll/**","/api/v1/**")
+			.securityMatcher("/admin/api/v1/**","/api/v1/**")
             .authorizeHttpRequests(
 				auth -> auth
-				.requestMatchers("/admin/api/v1/stroll/**","/api/v1/stroll").authenticated()
+				.requestMatchers("/admin/api/v1/**","/api/v1/stroll").authenticated()
 			)
 			.oauth2ResourceServer(
 						oauth2 -> oauth2.jwt(Customizer.withDefaults())
